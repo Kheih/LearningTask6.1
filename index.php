d577d2c1089985afb9e03a61860c3edfebd0f31f
Kheih <56195049+Kheih@users.noreply.github.com>
2020-11-23 18:33:06 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<svg width="900" height="780" style="border:1px solid black">
+
+
+<path id="b" d="M 330, 320 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "/>
+    </defs>
+        <use xlink:href="#b" fill="#fff"/>
+        <text fill="orange">
+            <textPath xlink:href="#b">Acircleisround, noend, that'showlongIwanttobeyour friend</textPath>
+        </text>
+    </g>
+
+<path id="c" d="M 430, 220 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "/>
+    </defs>
+        <use xlink:href="#c" fill="#fff"/>
+        <text fill="yellow">
+            <textPath xlink:href="#c">Lifeiscircle.Theend ofonejourneyisthebegginingofthenext.</textPath>
+        </text>
+    </g>
+
+<path id="d" d="M 560, 280 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "/>
+    </defs>
+        <use xlink:href="#d" fill="#fff"/>
+        <text fill="blue">
+            <textPath xlink:href="#d">Sometimesyourcircledecreasinginsize,butincreasesinvalue.</textPath>
+        </text>
+    </g>
+
+<path id="e" d="M 400, 450 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "/>
+    </defs>
+        <use xlink:href="#e" fill="#fff"/>
+        <text fill="indigo">
+            <textPath xlink:href="#e">myloveforyouisajourneystartingatforeverandendingatnever</textPath>
+        </text>
+    </g>
+
+
+<path id="f" d="M 540, 420 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "/>
+    </defs>
+        <use xlink:href="#f" fill="#fff"/>
+        <text fill="violet">
+            <textPath xlink:href="#f">Come out of the circle of time And into the circle of love. </textPath>
+        </text>
+    </g>
+
+  
+<path id="a" d="M 450, 340 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "/>
+    </defs>
+        <use xlink:href="#a" fill="#fff"/>
+        <text fill="red">
+            <textPath xlink:href="#a">WEAREALLCONNECTEDINTHEGREATCIRCLEOFLIFE</textPath>
+        </text>
+    </g>
+
+</svg>
